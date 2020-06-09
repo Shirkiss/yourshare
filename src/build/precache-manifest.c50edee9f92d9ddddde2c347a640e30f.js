b4a22dff5889e0d1ff6fbf6e344c48e046798c2d
shir.cohen <shir.cohen@myheritage.com>
2020-06-09 09:12:39 +0300

    #description [FE] Adding Contigo as the account management components #is_emergency #reviewer #tester #unit_tests #jira #flag #sensor #remark

---
+++ src/build/precache-manifest.c50edee9f92d9ddddde2c347a640e30f.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c573c4e56bbfd62f314",
+    "url": "/static/js/12.e0e3cd6f.chunk.js"
+  },
+  {
+    "revision": "0ba9b619191767f1241e",
+    "url": "/static/css/main.789cb61a.chunk.css"
+  },
+  {
+    "revision": "6df127f86bfbd76380c4",
+    "url": "/static/js/polyfills-css-shim.398068f7.chunk.js"
+  },
+  {
+    "revision": "2ff43ba3a25099193741",
+    "url": "/static/js/runtime~main.3162b3c9.js"
+  },
+  {
+    "revision": "4e55df0cce104855cc71",
+    "url": "/static/js/25.05609bf6.chunk.js"
+  },
+  {
+    "revision": "4c60b858bdc233d4f39d",
+    "url": "/static/js/3.a9f61f76.chunk.js"
+  },
+  {
+    "revision": "7743a0afecf1a1338781",
+    "url": "/static/js/4.f92947ab.chunk.js"
+  },
+  {
+    "revision": "82042c9b3937a6434c6c",
+    "url": "/static/js/5.aef30607.chunk.js"
+  },
+  {
+    "revision": "da21d96bbe707ac11682",
+    "url": "/static/js/6.f1706910.chunk.js"
+  },
+  {
+    "revision": "f56346be94e7fca4d6df",
+    "url": "/static/js/7.792fd49e.chunk.js"
+  },
+  {
+    "revision": "6e87b6f2311e917acf66",
+    "url": "/static/js/8.f45cbedf.chunk.js"
+  },
+  {
+    "revision": "220f8c2cbdd7b703fda2",
+    "url": "/static/js/9.2e356c20.chunk.js"
+  },
+  {
+    "revision": "2b8f3b1095b8dae487ec",
+    "url": "/static/js/10.b1894998.chunk.js"
+  },
+  {
+    "revision": "cc734b849a7ecdc307cf",
+    "url": "/static/js/11.144f38f2.chunk.js"
+  },
+  {
+    "revision": "0ba9b619191767f1241e",
+    "url": "/static/js/main.ca13121b.chunk.js"
+  },
+  {
+    "revision": "48817a391978d3af1d25",
+    "url": "/static/js/13.a99d062c.chunk.js"
+  },
+  {
+    "revision": "b207c52f842042155f5e",
+    "url": "/static/js/14.4764465b.chunk.js"
+  },
+  {
+    "revision": "bdb829056e55a73c41e3",
+    "url": "/static/js/15.3a1bc3d0.chunk.js"
+  },
+  {
+    "revision": "85f26903cfec93479fb9",
+    "url": "/static/js/16.ee805447.chunk.js"
+  },
+  {
+    "revision": "6ce90cf98f39c0c433df",
+    "url": "/static/js/17.429ae4e9.chunk.js"
+  },
+  {
+    "revision": "70dbc4be7252ceea6927",
+    "url": "/static/js/18.b0423be2.chunk.js"
+  },
+  {
+    "revision": "3c6d85d0db86dca81f2e",
+    "url": "/static/js/19.9aa0d7fe.chunk.js"
+  },
+  {
+    "revision": "9f85fb2840a0c9cc357f",
+    "url": "/static/js/20.ac34b986.chunk.js"
+  },
+  {
+    "revision": "b802318cf6bd8a090abe",
+    "url": "/static/js/21.a6e0b441.chunk.js"
+  },
+  {
+    "revision": "64d5ad8272e2dd0afafa",
+    "url": "/static/js/22.1faab674.chunk.js"
+  },
+  {
+    "revision": "00a649e2cb0ef409127a",
+    "url": "/static/js/23.6f4f0ace.chunk.js"
+  },
+  {
+    "revision": "6e226ef4cc123bd0a61b",
+    "url": "/static/js/24.8ee6e140.chunk.js"
+  },
+  {
+    "revision": "4c60b858bdc233d4f39d",
+    "url": "/static/css/3.15b595cf.chunk.css"
+  },
+  {
+    "revision": "592eeb4335975998ab814847945d18ca",
+    "url": "/index.html"
+  }
+];
